4a4787c1bb0d95eaec6e6ac9c5fecbae1858bdb3
sraybuck <suzraybuck@gmail.com>
2019-10-23 12:38:50 -0400

    using express

---
+++ express-demo.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+//var $ = require('jQuery');
+
+var { Liquid } = require('liquidjs');
+var engine = new Liquid();
+
+var app = express();
+
+app.engine('liquid', engine.express());
+
+app.set('views', path.resolve(__dirname, "views"));
+
+app.set('view engine', 'liquid');
+
+app.use(express.static('static'));
+
+app.get("/", (req, res) => {
+    let page = $.getJSON("index.json")
+    res.render("base.liquid", {content : page[1]});
+});
+
+
+app.get("/test", (req, res) => {
+
+    //let today = new Date();
+    res.render("page.liquid", {content: "filename"});
+});
+
+app.use((req, res) => {
+    res.statusCode = 404;
+    res.end("404 - page not found");
+});
+
+app.listen(3000, () => {
+
+    console.log("Application started on port 3000");
+})
