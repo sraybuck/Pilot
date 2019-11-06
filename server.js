957df8c362668acbd074b3468a47c6e718b90913
sraybuck <suzraybuck@gmail.com>
2019-11-06 15:57:23 -0500

    whitespace changes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 //load filesystem module that allows you to read directories and files within the app
 const fs = require('fs');
 //load liquid module
+
 var { Liquid } = require('liquidjs');
 //create engine object using liquid
 var engine = new Liquid();
