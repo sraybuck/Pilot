010ee72eecff16478b4e4b60f385a3700bbf3ba1
sraybuck <suzraybuck@gmail.com>
2019-11-20 10:38:57 -0500

    commenting

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require("path");
 //load filesystem module that allows you to read directories and files within the app
 const fs = require('fs');
 //load liquid module
-
 var { Liquid } = require('liquidjs');
 //create engine object using liquid
 var engine = new Liquid();
@@ -62,9 +61,11 @@ app.get("/collection", (req, res) => {
   res.render("toc.liquid", {content: "Collection", files: fs.readdirSync("site/collection/")});
 });
 
-//iterate through collection folder and generate a new page for each TEI file
-//the ":" specifies a dummy variable "file" that represents each item that matches that pattern inside the collection folder
-//within the liquid brackets, 'req.params.file+".xml"'' refers back to the file dummy variable and appends a .xml extension to it to get the right filename to pass to CETEIcean
+/*iterate through collection folder and generate a new page for each TEI file
+
+The ":" specifies a dummy variable "file" that represents each item that matches that pattern inside the collection folder.
+
+Within the liquid brackets, 'req.params.file+".xml"'' refers back to the file dummy variable and appends a .xml extension to it to get the right filename to pass to CETEIcean*/
 app.get('/collection/:file', (req, res) => {
   res.render("page.liquid", {filename: req.params.file+".xml"});
 });
