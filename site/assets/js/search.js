00f455bd84f382164333ec9912d01a4ca34aec4a
sraybuck <suzraybuck@gmail.com>
2020-01-15 10:43:24 -0500

    basic search with dummy data working

--- site/assets/js/search.js
+++ site/assets/js/search.js
@@ -1,19 +1,42 @@
 // Based on a script by Kathie Decora : katydecorah.com/code/lunr-and-jekyll/
-
-
-
 //create the lunr index
 var index = elasticlunr(function () {
   this.addField('title')
   this.addField('content')
+  this.setRef('id');
 });
 
-var files = {}
-files.push("lINK OUT TO OTHER TEXT FILE");
-//add the content to the index
-//ITERATE
+//hard coded search doc
+var doc1 = {
+  "id": 1,
+  "title": "Anthem",
+  "body": "Anthem for Doomed Youth is a good poem."
+}
 
-/*index.addDoc({
-  title: {{}}
-});*/
-//liquid tags????
+index.addDoc(doc1);
+//search doc from variable outside
+
+
+//search doc from file
+
+console.log(index)
+
+function doSearch() {
+  var resultdiv = $('#results');
+  var query = $('input#search').val();
+
+  var result = index.search(query);
+  
+  resultdiv.empty();
+  if (result.length == 0) {
+    resultdiv.append('<p class="">No results found.</p>');
+  } else if (result.length == 1) {
+    resultdiv.append('<p class="">Found '+result.length+' result</p>');
+  } else {
+    resultdiv.append('<p class="">Found '+result.length+' results</p>');
+  }
+}
+
+$(document).ready(function() {
+  $('input#search').on('keyup', doSearch);
+});
